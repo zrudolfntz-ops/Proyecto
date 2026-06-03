a625c4dc5b30a47c5bc92b681c10bb523a3dfe87
Erick Hilario <ershp.230@gmail.com>
2026-06-03 16:16:05 -0600

    Update Abastecimiento.cs

--- ProyectoBombas/BLL/Entidades/Abastecimiento.cs
+++ ProyectoBombas/BLL/Entidades/Abastecimiento.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace BLL.Entidades
 {
-    internal class Abastecimiento
+    public class Abastecimiento
     {
+        private int id;
+        private Cliente cliente;
+        private Bomba bomba;
+        private decimal litros;
+        private decimal total;
+        private DateTime fechaHora;
+        private string tipo;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public Cliente Cliente
+        {
+            get { return cliente; }
+            set { cliente = value; }
+        }
+
+        public Bomba Bomba
+        {
+            get { return bomba; }
+            set { bomba = value; }
+        }
+
+        public decimal Litros
+        {
+            get { return litros; }
+            set { litros = value; }
+        }
+
+        public decimal Total
+        {
+            get { return total; }
+            set { total = value; }
+        }
+
+        public DateTime FechaHora
+        {
+            get { return fechaHora; }
+            set { fechaHora = value; }
+        }
+
+        public string Tipo
+        {
+            get { return tipo; }
+            set { tipo = value; }
+        }
     }
 }
