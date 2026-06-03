1aa3b658b9ec2ecee6b484dd64b9b775026fc3df
zrudolfntz-ops <ershp.230@gmail.com>
2026-06-03 16:14:47 -0600

    Creación de entidades
    Se crearon las clases de las entidades a usar dentro del proyecto

---
+++ ProyectoBombas/BLL/Entidades/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Entidades
+{
+    public class Cliente
+    {
+        private string nombre;
+
+        public Cliente()
+        {
+        }
+
+        public Cliente(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+    }
+}
