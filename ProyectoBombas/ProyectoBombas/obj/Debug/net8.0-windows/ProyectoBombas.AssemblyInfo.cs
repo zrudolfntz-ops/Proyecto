10532f73c5249af9981a373283ad480322e34c31
Rodolfo Morales <zrudolfntz@gmail.com>
2026-06-04 12:53:37 -0600

    Creación de los User Controls
    Se crearon 6 user control para el mostrar cada modulo del sistema

--- ProyectoBombas/ProyectoBombas/obj/Debug/net8.0-windows/ProyectoBombas.AssemblyInfo.cs
+++ ProyectoBombas/ProyectoBombas/obj/Debug/net8.0-windows/ProyectoBombas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoBombas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81a6792b9d8e22eef2de7c973493b0ceda1cdc91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cb81923dafa987a60ce21b56c5e3241e2da8f87")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoBombas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoBombas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
